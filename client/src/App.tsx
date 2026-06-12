95c3c44619001259e4d1c9e12419987523cc6b48
utkarsharsh <harshupadhyay7786@gmail.com>
2026-06-12 21:36:21 +0530

    api integration done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import { Layout } from "lucide-react";
-import AiComposer from "./pages/AiComposer";
-import Accounts from "./pages/Accounts";
+import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
+import Accounts from "./pages/Accounts";
 import Scheduler from "./pages/Scheduler";
+import AIComposer from "./pages/AIComposer";
+import { Toaster } from "react-hot-toast";
+
 export default function App() {
     return (
         <>
+            <Toaster position="top-right"/>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route element={<Layout />}>
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/accounts" element={<Accounts />} />
-                        <Route path="/scheduler" element={<Scheduler />} />
-                    <Route path="/ai-composer" element={<AiComposer />} />
+                    <Route path="/dashboard" element={<Dashboard />}/>
+                    <Route path="/accounts" element={<Accounts />}/>
+                    <Route path="/schedule" element={<Scheduler />}/>
+                    <Route path="/ai-composer" element={<AIComposer />}/>
+
                 </Route>
             </Routes>
         </>
